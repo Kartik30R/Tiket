c749d5dcb95477e778146dd462235316de564b71
kartik30R <kartik30rehan@gmail.com>
2025-10-29 01:10:44 +0530

    add:auth middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Kartik30R/Tiket.git/config"
 	"github.com/Kartik30R/Tiket.git/db"
 	"github.com/Kartik30R/Tiket.git/handlers"
+	"github.com/Kartik30R/Tiket.git/middleware"
 	"github.com/Kartik30R/Tiket.git/repositories"
 	"github.com/Kartik30R/Tiket.git/services"
 	"github.com/gin-gonic/gin"
@@ -21,7 +22,7 @@ func main() {
    }
 
 router := gin.Default()
-   authRepo := repositories.NewAuthRepository()
+   authRepo := repositories.NewAuthRepository(db)
    eventRepo := repositories.NewEventRepository(db)
    ticketRepo := repositories.NewTicketRepository(db)
 
@@ -29,11 +30,11 @@ router := gin.Default()
 
    server := router.Group("/api")
       handlers.NewAuthHandler(server.Group("/auth"),authService)
-	  privateRoute:=server.Use(middleware.AuthProtected(db))
-
+ protected := server.Group("/")  
+protected.Use(middleware.AuthProtected(db)) 
    
-   handlers.NewEventHandler(server.Group("/event"),eventRepo)
-      handlers.NewTicketHandler(server.Group("/ticket"),ticketRepo)
+   handlers.NewEventHandler(protected.Group("/event"),eventRepo)
+      handlers.NewTicketHandler(protected.Group("/ticket"),ticketRepo)
 
 
 router.Run(":" + cfg.ServerPort)
--- middleware/auth.go
+++ middleware/auth.go
@@ -1,3 +1,72 @@
 package middleware
 
-func AuthProtected()
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/gorm"
+)
+
+func AuthProtected(db *gorm.DB) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		autherizationHeader := ctx.GetHeader("authorization")
+       if len(autherizationHeader)==0{
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "fail",
+			"message": "Unauthorize",
+		})
+		ctx.Abort()
+			return
+	   }
+
+	   tokenParts := strings.Split(autherizationHeader," ")
+
+	   if len(tokenParts)!=2 || tokenParts[0] != "Bearer"{
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "fail",
+			"message": "Unauthorize",
+		})
+		ctx.Abort()
+			return
+	   }
+
+	   tokenstr:= tokenParts[1]
+	   secret := []byte(os.Getenv("SECRET"))
+
+	token, err := jwt.Parse(tokenstr, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return secret, nil
+		})
+
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "fail",
+				"message": "Invalid or expired token",
+			})
+			ctx.Abort()
+			return
+		}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "fail",
+				"message": "Invalid token claims",
+			})
+			ctx.Abort()
+			return
+		}
+
+
+        userId := claims["id"]
+		ctx.Set("userId", userId)	
+	
+
+		ctx.Next()
+	}
+}
